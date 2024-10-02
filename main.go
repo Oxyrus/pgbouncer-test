9c054185b03548df97a40e82f7476e99a22ba49b
Oxyrus <im.oxyrus@gmail.com>
2024-10-01 19:21:57 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connStr := "postgres://user:password@localhost:6432/mydb?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, name FROM test_table")
+	if err != nil {
+		log.Fatal("Failed to execute query:", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %d, Name: %s\n", id, name)
+	}
+}
